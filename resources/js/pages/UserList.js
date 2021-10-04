3ec5a43fcd8a4dedaa12d3437aa56c9d8ffc975a
kbyukita <ki.ta.ya.0713@gmail.com>
2021-10-04 21:10:09 +0900

    COMMIT-32 fixed design in userlist

--- resources/js/pages/UserList.js
+++ resources/js/pages/UserList.js
@@ -1,26 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
-import { IconButton, Container } from '@material-ui/core';
+import { IconButton, Container, Box, Chip, Badge, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory, useParams } from 'react-router-dom';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import Loading from './Loading';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        backgroundColor: theme.palette.background.paper,
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh'
     },
     inline: {
         display: 'inline',
     },
+    replied: {
+        display: 'none'
+    },
+    notReplied: {
+        marginLeft: 'auto'
+    },
+    userBox: {
+        display: 'flex',
+        alignItems: 'center',
+        magrin: '4px',
+        "&:hover": {
+            backgroundColor: "#e0e9f2"
+        },
+        "&:active": {
+            background: "#e0e9f2"
+        }
+    }
 }));
 
 const UserList = () => {
@@ -68,42 +81,79 @@ const UserList = () => {
     }
 
     return (
-        <Container>
-            <div className="card">
-                <div className="card-header">
-                    <IconButton onClick={() => history.goBack()} ><ArrowBackIosIcon /></IconButton>
+        <Container className={classes.container}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                }}>
+                <IconButton onClick={() => history.goBack()} ><ArrowBackIosIcon /></IconButton>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}>
                     {officialAccount.name} トーク
-                </div>
-                <List className={classes.root}>
-                    {userList.map((item, index) => (
-                        <div key={index}>
-                            <ListItem button onClick={() => history.push({ pathname: '../chat', state: { lineUser: item, officialAccount: officialAccount } })} alignItems="flex-start">
-                                <ListItemAvatar>
-                                    <Avatar alt="Remy Sharp" src={item.user_picture_url} />
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary={item.displayname}
-                                    secondary={
-                                        <React.Fragment>
-                                            {/* <Typography
-                                    component="span"
-                                    variant="body2"
-                                    className={classes.inline}
-                                    color="textPrimary"
-                                >
-                                    Ali Connors
-                                </Typography> */}
-                                            {item.current_chat}
-                                        </React.Fragment>
-                                    }
-                                />
-                            </ListItem>
+                </Box>
+            </Box>
 
-                        </div>
-                    ))}
-                </List>
-            </div>
-        </Container>
+            {userList.map((item, index) => (
+                <Box
+                    className={classes.userBox}
+                    key={index}
+                    onClick={() =>
+                        history.push({
+                            pathname: '../chat',
+                            state: { lineUser: item, officialAccount: officialAccount }
+                        })}>
+                    <Box p={1}>
+                        <Badge
+                            invisible={Boolean(item.is_read_by_admin)}
+                            color="secondary"
+                            variant="dot">
+                            <Avatar alt={item.displayname} src={item.user_picture_url} />
+                        </Badge>
+                    </Box>
+                    <Box
+                        sx={{
+                            flex: 1,
+                            minWidth: 0
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                width: '100%'
+                            }}>
+                            <Box>
+                                {item.displayname}
+                            </Box>
+                            <Box ml={1}>
+                                {item.updated_at}
+                            </Box>
+                            <Chip
+                                className={item.is_replied_by_admin == true ? classes.replied : classes.notReplied}
+                                label="要返信"
+                                color="secondary"
+                                variant="outlined"
+                                size="small" />
+                        </Box>
+                        <Box
+                            sx={{
+                                width: '100%',
+                                overflow: 'hidden',
+                                whiteSpace: 'nowrap',
+                                textOverflow: 'ellipsis'
+                            }}>
+                            {item.current_chat}
+                        </Box>
+                    </Box>
+                </Box>
+            ))
+            }
+        </Container >
+
     );
 }
 
